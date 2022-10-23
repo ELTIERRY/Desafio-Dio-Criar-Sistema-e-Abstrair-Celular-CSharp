61a318185d30e79a28816d238dd2f738fe101d28
Eltierry <eltierry@gmail.com>
2022-10-23 10:20:49 -0300

    Primeiro commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using DesafioPOO.Models;
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia (numero:"111111",modelo: "Modelo 1", imei:"imeixxxxx", memoria: 256);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine();
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone (numero:"22222222",modelo: "Modelo 2", imei:"imeiyyyyyyy", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
